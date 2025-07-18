1ceabdea72b611baeb802a9fe57d3645a70c23db
Davedaz23 <Davegbhonesty@gmail.com>
2025-07-17 20:44:53 -0700

    defaru

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  resolver: {
-    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
-  },
-};
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Merge your custom resolver settings
+defaultConfig.resolver.assetExts = [
+  ...defaultConfig.resolver.assetExts, // Keep Expo's defaults
+  'png', 'jpg', 'jpeg', 'gif', 'svg', // Add your extensions
+];
+
+module.exports = defaultConfig;
